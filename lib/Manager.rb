070c596b6e9972223e57da303d408ebafe8fb2d9
Neal JIn Oh <no4259@gmail.com>
2019-09-22 00:19:16 +0000

    Automatically backed up by Learn

--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,31 @@
 class Manager
+  
+  attr_accessor :name, :department
+  @@all = []
+  
+  def initialize(name, department)
+    @name = name
+    @department = department
+  end
+  
+  def age
+  end
+  
+  def employees
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  
+  def hire_employee
+  end
+
+  def self.all_departments
+  end
+  
+  def self.average_age
+  end
 
 end
